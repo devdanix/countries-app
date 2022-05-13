669b4ca4b6811bd3823b435b8f64a83d06ae9bfb
Daniele Loi <daniele.loi@titanemail.com>
2022-05-13 10:56:39 +0100

    moved a inside li and added item-txt class

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -10,18 +10,18 @@ export default function CountryItem({ country }) {
 
 
   return (
-    <Link to={`/details/${country.name.toLowerCase()}`}>
-      <li className={`country-listitem item-${theme.toLowerCase()} shadow-${theme.toLowerCase()}`}>
-        <img className='country-listitem-img' src={country.flags.png} alt={`${country.name}_flag`}/>
-        <div className='country-listitem-info'>
-          <p className='country-listitem-name'>{country.name}</p>
-          <ul>
-            <li><p className='fw-600'>Population: <span className='fw-300'>{country.population.toLocaleString()}</span></p></li>
-            <li><p className='fw-600'>Region: <span className='fw-300'>{country.region}</span></p></li>
-            <li><p className='fw-600'>Capital: <span className='fw-300'>{country.capital}</span></p></li>
-          </ul>
-        </div>
-      </li>
-    </Link>
+    <li className={`country-listitem item-${theme.toLowerCase()} shadow-${theme.toLowerCase()}`}>
+      <Link to={`/details/${country.name.toLowerCase()}`} className={`item-txt-${theme.toLowerCase()}`}>
+          <img className='country-listitem-img' src={country.flags.png} alt={`${country.name}_flag`}/>
+          <div className='country-listitem-info'>
+            <p className='country-listitem-name'>{country.name}</p>
+            <ul>
+              <li><p className='fw-600'>Population: <span className='fw-300'>{country.population.toLocaleString()}</span></p></li>
+              <li><p className='fw-600'>Region: <span className='fw-300'>{country.region}</span></p></li>
+              <li><p className='fw-600'>Capital: <span className='fw-300'>{country.capital}</span></p></li>
+            </ul>
+          </div>
+      </Link>
+    </li>
   )
 }
